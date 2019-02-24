f13316e2962e94044ea11d1cd3af38d87765f59e
v <bingcheng94@gmail.com>
2019-02-25 01:52:50 +0800

    1

---
+++ huyaapi副本.js
@@ -0,0 +1,31 @@
+var md5=require("md5-node")
+var data='{"roomId":11279251}'
+var appId='154751727055855148'
+var key='*********'
+var time = Math.round(new Date().getTime()/1000)
+var sign = md5('data='+data+'&key='+key+'&timestamp='+time)
+var WebSocket = require('ws')
+var socket = new WebSocket("ws://openapi.huya.com/index.html?do=getMessageNotice&data={\"roomId\":11279251}&appId="+appId+"&timestamp="+time+"&sign="+sign);
+
+
+socket.onopen=function(event)
+{
+    socket.send('ping');
+    setInterval(function(){
+        socket.send('ping');
+    },15000);
+};
+//处理到来的信令
+socket.onmessage = function(event){
+    console.log('onmessage: ',event.data);
+    var json = JSON.parse(event.data);
+    if (json.statusCode == 200)
+    {
+        //TODO处理弹幕数据json.data
+        // document.write('['+json.data.sendNick+']: '+json.data.content+'');
+    }
+    else
+    {
+        //TODO 错误处理
+    }
+};
